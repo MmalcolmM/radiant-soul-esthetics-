9543cca7ad2d1e4e88a0c872ffc3e440b88accc8
Malcolm Franklin <mac.mac5@yahoo.com>
2024-08-02 15:01:39 -0600

    updated server again

--- server/schemas/index.js
+++ server/schemas/index.js
@@ -1,8 +1,4 @@
 const typeDefs = require('./typeDefs');
 const resolvers = require('./resolvers');
 
-<<<<<<< HEAD
 module.exports = { typeDefs, resolvers };
-=======
-module.exports = { typeDefs, resolvers };
->>>>>>> 729d12bb27b436e236de5a871ff4c0c9a6cd9fba
--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,39 +1,3 @@
-<<<<<<< HEAD
-const { Query } = require('mongoose');
-const { Service } = require('../models');
-const { AuthenticationError } =  require('apollo-server-express');
-const jwt = require('jsonwebtoken');
-
-
-const signToken = (user) => {
-    return jwt.sign(
-        { id: user._id, email: user.email, isAdmin: user.isAdmin},
-        process.env.JWT_Secret,
-        {expiresIn: '1h'}
-    );
-};
-
-const resolvers = {
-    Query: {
-        getServices: async () => {
-            return await Service.find()
-        },
-        getService: async (_, { id }) => {
-            return await Service.findById(id);
-        },
-    },
-
-    Mutation: {
-        addService: async (parent, { name, description, price}, context) => {
-            if (!context.user || !context.user.isAdmin ) {
-                throw new AuthenticationError('Unauthorized');
-            }
-            const service = new Service({ name, description, price });
-            return await service.save()
-        }
-    }
-}
-=======
 const {User} = require('../models')
 
 const resolvers = {
@@ -71,4 +35,3 @@ const resolvers = {
   };
 
 module.exports = resolvers;
->>>>>>> 729d12bb27b436e236de5a871ff4c0c9a6cd9fba
