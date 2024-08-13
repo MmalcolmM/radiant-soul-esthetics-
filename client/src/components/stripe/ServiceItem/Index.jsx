87929d6a1f1ae71e72f4dd5d09d393ed1845932b
cnm724 <cnm724@gmail.com>
2024-08-13 05:43:51 -0600

    Stripe functionality, changed size of admin and login button, added a user seed

--- client/src/components/stripe/ServiceItem/Index.jsx
+++ client/src/components/stripe/ServiceItem/Index.jsx
@@ -1,44 +1,17 @@
-import { Link } from "react-router-dom";
-import { useStoreContext } from "../../../utils/GlobalState";
-import { ADD_TO_CART } from "../../../utils/actions";
-import { idbPromise } from "../../../utils/helpers";
+function ServiceItem({ title, _id, price, link }) {
 
-function ServiceItem(item) {
-  const [state, dispatch] = useStoreContext();
-
-  const {
-    title,
-    _id,
-    price
-  } = item;
-
-  const { cart } = state
-
-  const addToCart = () => {
-    const itemInCart = cart.find((cartItem) => cartItem._id === _id);
-    if (itemInCart) {
-      // If item is already in the cart, do nothing since it can only be purchased once
-      console.log('Item already in cart');
-    } else {
-      // If item is not in the cart, add it with purchaseQuantity set to 1
-      dispatch({
-        type: ADD_TO_CART,
-        product: { ...item, purchaseQuantity: 1 }
-      });
-      idbPromise('cart', 'put', { ...item, purchaseQuantity: 1 });
-    }
-  }
+  const handlePurchase = () => {
+    window.open(link, '_blank');
+  };
 
   return (
     <div className="card px-1 py-1">
-      <Link to={`/service/${_id}`}>
         <p>{title}</p>
-      </Link>
       <div>
         <div>Purchase {title} service!</div>
         <span>${price}</span>
       </div>
-      <button onClick={addToCart}>Add to cart</button>
+      <button onClick={handlePurchase}>Book Service</button>
     </div>
   );
 }
--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -24,25 +24,35 @@ const signToken = (user) => {
 
 const resolvers = {
   Query: {
+    users: async () => {
+      return await User.find();
+    },
     user: async (_, __, context) => {
       if (context.user) {
-        const user = await User.findById(context.user.id);
-
-        user.orders.sort((a, b) => b.purchaseDate - a.purchaseDate);
-
+        const user = await User.findById(context.user._id)
+          .populate({
+            path: 'orders',
+            populate: {
+              path: 'services', // Populate services within orders
+            },
+          });
+    
+        if (!user) {
+          throw new AuthenticationError('User not found');
+        }
+    
         return user;
       }
-
       throw new AuthenticationError('You must be logged in');
     },
+
     getServices: async () => {
       return await Service.find();
-      console.log('Services from DB:', services)
     },
     getService: async (_, { _id }) => {
       return await Service.findById(_id);
     },
-    order: async (parent, { _id }, context) => {
+    user: async (parent, { _id }, context) => {
       if (context.user) {
         const user = await User.findById(context.user._id);
 
@@ -51,95 +61,65 @@ const resolvers = {
 
       throw new AuthenticationError('You must be logged in');
     },
-    // checkout: async (parent, args, context) => {
-    //   const url = new URL(context.headers.referer).origin;
-    //   const order = new Order({ services: args.services });
-    //   const line_items = [];
+    checkout: async (parent, args, context) => {
+      const url = new URL(context.headers.referer).origin;
+      const order = new Order({ services: args.services });
+      const line_items = [];
 
-    //   const { services } = await order.populate('services');
+      const { services } = await order.populate('services');
 
-    //   for (let i = 0; i < services.length; i++) {
-    //     const service = services[i];
-    //     line_items.push({
-    //       price_data: {
-    //         currency: 'usd',
-    //         product_data: {
-    //           name: service.title,
-    //           description: service.description,
-    //         },
-    //         unit_amount: service.price * 100,
-    //       },
-    //       quantity: 1,
-    //     });
-    //   }
-
-    //   const session = await stripe.checkout.sessions.create({
-    //     payment_method_types: ['card'],
-    //     line_items,
-    //     mode: 'payment',
-    //     success_url: `${url}/success?session_id={CHECKOUT_SESSION_ID}`,
-    //     cancel_url: `${url}/`,
-    //   });
-
-    //   return { session: session.id };
-    // }
-    checkout: async (_, { services }) => {
-      // Fetch the service details and create line items
-      const lineItems = services.map(serviceId => {
-        // Replace this comment with actual code to fetch the service from your database
-        const service = { title: 'Service title', price: 10000 }; // Example service object
-
-        return {
+      for (let i = 0; i < services.length; i++) {
+        const service = services[i];
+        line_items.push({
           price_data: {
             currency: 'usd',
             product_data: {
               name: service.title,
+              description: service.description,
             },
-            unit_amount: service.price,
+            unit_amount: service.price * 100,
           },
           quantity: 1,
-        };
-      });
+        });
+      }
 
-      // Create the Stripe checkout session
       const session = await stripe.checkout.sessions.create({
         payment_method_types: ['card'],
-        line_items: lineItems,
+        line_items,
         mode: 'payment',
-        success_url: `${process.env.YOUR_DOMAIN}/success?session_id={CHECKOUT_SESSION_ID}`,
-        cancel_url: `${process.env.YOUR_DOMAIN}/cancel`,
+        success_url: `${url}/success?session_id={CHECKOUT_SESSION_ID}`,
+        cancel_url: `${url}/`,
       });
 
-      // Return the session ID
       return { session: session.id };
     },
   },
   Mutation: {
     signup: async (_, { name, email, password }) => {
       try {
-      console.log('Signup mutation called');
-      console.log('Name:', name);
-      console.log('Email:', email);
-      console.log('Password:', password);
+        console.log('Signup mutation called');
+        console.log('Name:', name);
+        console.log('Email:', email);
+        console.log('Password:', password);
 
-      const existingUser = await User.findOne({ email });
-      if (existingUser) {
-        throw new AuthenticationError('User already exists');
-      }
-    
+        const existingUser = await User.findOne({ email });
+        if (existingUser) {
+          throw new AuthenticationError('User already exists');
+        }
+
         const newUser = new User({ name, email, password });
         await newUser.save();
         console.log('User saved successfully');
         const token = signToken(newUser);
         return token;
       } catch (error) {
-        
+
         console.error('Error saving user:', error.message);
         throw new Error('Error saving user');
         return
       }
 
-  
+
     },
     login: async (_, { email, password }) => {
       const user = await User.findOne({ email });
@@ -172,7 +152,7 @@ const resolvers = {
           html: `<p><strong>Name:</strong> ${name}</p><p><strong>Email:</strong> ${email}</p><p><strong>Message:</strong> ${message}</p>`,
         };
         await sgMail.send(msgToSelf);
-    
+
         // Automatic reply to the user
         const autoReplyMsg = {
           to: email,
@@ -181,10 +161,10 @@ const resolvers = {
           text: `Hello ${name},\n\nThank you for reaching out to Radiant Soul Esthetics! Your inquiry is important to us.\n\nPlease note that I am currently away from my email, attending to clients. I will do my best to respond to your message within 24-48 hours.\n\nFor urgent inquiries, feel free to call or text me directly at 303-550-9603 or to schedule an appointment, please visit rsethetics.com.\n\nThank you for your patience and understanding.\n\nLove and Light,\n\nDeidre Hallert\nPMU Artist and Esthetician\nRadiant Soul Esthetics\n\n303-550-9603\ninfo@rsesthetics.com`,
           html: `<p>Hello ${name},</p><p>Thank you for reaching out to Radiant Soul Esthetics! We have received your message and will get back to you as soon as possible.</p><p>Best regards,<br>Radiant Soul Esthetics Team</p>`,
         };
-    
+
         const response = await sgMail.send(autoReplyMsg);
         console.log('SendGrid Response:', response);
-    
+
         return 'Email sent successfully!';
       } catch (error) {
         console.error('Error sending email:', error);
@@ -215,18 +195,16 @@ const resolvers = {
       if (!context.user || !context.user.isAdmin) {
         throw new AuthenticationError('Unauthorized');
       }
-      
-      return await Service.findOneAndDelete({_id: id}) ;
+
+      return await Service.findOneAndDelete({ _id: id });
     },
     addOrder: async (parent, { services }, context) => {
       if (context.user) {
         const order = new Order({ services });
-
-        await User.findByIdAndUpdate(context.user._id, { $push: { orders: order } });
-
+        await order.save(); // Explicitly save the order
+        await User.findByIdAndUpdate(context.user._id, { $push: { orders: order._id } }); // Save only the order ID to the user
         return order;
       }
-
       throw new AuthenticationError('You must be logged in');
     },
   },
